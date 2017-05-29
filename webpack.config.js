d936989006710ebd6b5ffebe79eb1443e63f4d60
westerp <sylwester.git@sylwester.no>
2017-05-29 15:35:27 +0200

    Using variable works

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
                     },
                     {
                         loader: 'ejs-loader',
+                        query: {
+                            variable: 'o',
+                        }
                     },
                 ],
             },
